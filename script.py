5d82e7fe69e1e4691f9b31877c4c028f3456d0d9
Mia <mobrien@stuy.edu>
2017-06-13 05:51:12 -0400

    Create script.py

--- script.py
+++ script.py
@@ -47,20 +47,38 @@ def first_pass( commands ):
   appropirate value. 
   ===================="""
 def second_pass( commands, num_frames):
-    knobs = []
-    prog = 0
-    while prog < num_frames:
-        knobs.append({})
-        prog+=1
-    i = 0
-    for c in commands:
-        if c[0] == "vary":
-            for i in range (int(c[2]), int(c[3])+1):
-                if float(c[5]) > float(c[4]):
-                    knobs[i][c[1]] = float(c[5]-c[4])/(int(c[3])-int(c[2])+1)*(i-int(c[2]) +1)
-                else:
-                    knobs[i][c[1]] = float(c[4]) - float(c[4]-c[5])/(int(c[3]) - int(c[2]) + 1)*(i - int(c[2]) + 1)
-    return knobs
+    frames = [ {} for i in range(num_frames) ]
+    cons = {};
+    sources = {};
+    amb = {};
+    shading = "none"
+
+    for command in commands:
+        if command[0] == 'vary':
+            knob_name = command[1]
+            start_frame = command[2]
+            end_frame = command[3]
+            start_value = float(command[4])
+            end_value = float(command[5])
+            value = 0
+
+            if ((start_frame < 0) or
+                (end_frame >= num_frames) or
+                (end_frame <= start_frame)):
+                print 'Invalid vary command for knob: ' + knob_name
+                exit()
+
+            delta = (end_value - start_value) / (end_frame - start_frame)
+
+            for f in range(num_frames):
+                if f == start_frame:
+                    value = start_value
+                    frames[f][knob_name] = value
+                elif f >= start_frame and f <= end_frame:
+                    value = start_value + delta * (f - start_frame)
+                    frames[f][knob_name] = value
+                #print 'knob: ' + knob_name + '\tvalue: ' + str(frames[f][knob_name])
+    return frames
 
 
 def run(filename):
@@ -78,6 +96,12 @@ def run(filename):
     (basename, num_frames) = first_pass(commands)
     anime = num_frames > 1
     knobs = second_pass(commands, num_frames)
+
+    #print symbols
+    sources = [symbols[i][1] for i in symbols if symbols[i][0] = "light"]
+    if "shading" in symbols:
+        shading = symbols["shading"][1];
+        
     for frame in range(num_frames):
 
       tmp = new_matrix()
@@ -86,6 +110,8 @@ def run(filename):
       stack = [ [x[:] for x in tmp] ]
       print "stack", stack[-1]
       screen = new_screen()
+      zb = new_zbuffer()
+      clear_zbuffer(zb);
       tmp = []
       step = 0.1
       val = 1  
@@ -100,7 +126,7 @@ def run(filename):
                       args[0], args[1], args[2],
                       args[3], args[4], args[5])
               matrix_mult( stack[-1], tmp )
-              draw_polygons(tmp, screen, color)
+              draw_polygons(tmp, zb, screen, color, symbols[command[-1]][1], sources, shading))
               tmp = []
           elif c == 'sphere':
               add_sphere(tmp,
@@ -114,6 +140,14 @@ def run(filename):
               matrix_mult( stack[-1], tmp )
               draw_polygons(tmp, screen, color)
               tmp = []
+              
+          elif c == 'set':
+              symbols[args[0]][1] = args[1]
+          elif c == 'set_knobs':
+              for knob in symbols:
+                if symbols[knob][0] == 'knob':
+                  symbols[knob][1] = args[0]
+                  
           elif c == 'move':
               if anime and args[3] != None:
                       knob = args[3]
