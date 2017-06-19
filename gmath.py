bef910a57e4c67f4f7ef1fa608ceb943edbed7f0
Mia <mobrien@stuy.edu>
2017-06-19 15:07:47 -0400

    Update gmath.py

--- gmath.py
+++ gmath.py
@@ -42,16 +42,78 @@ def dot_product(v_one, v_two):
     v_two = normalize(v_two)
     return v_one[0] * v_two[0] + v_one[1] * v_two[1] + v_one[2] * v_two[2]
 
-def ambient_color(color, ka):
-    return color * ka
+def colorize(normal, shading, color):
+    ambient = ambient_color(shading, color)
+    diffuse = diffuse_color(normal, shading, color)
+    specular = specular_color(normal, shading, color)
+    print ambient, '|', diffuse, '|', specular
+    I = [ambient[0] + diffuse[0] + specular[0],
+         ambient[1] + diffuse[1] + specular[1],
+        ambient[2] + diffuse[2] + specular[2]]
+    return [int(max(min(x, 255), 0)) for x in I]
 
-def diffuse_color(color, kd, normal, light_vector):
-    return color * kd * max(0, dot_product(normal, light_vector))
+def ambient_color(shading, color):
+    src_color = shading["ambient"]
+    const = shading["constants"]["constants"]
+    amb_const = [ const["red"][0], const["green"][0], const["blue"][0] ]
+    #color * ka 
+    ambient = [ amb_const[0] * src_color[0], #R
+                amb_const[1] * src_color[1], #G
+                amb_const[2] * src_color[2] ] #B
+    return ambient 
 
-def specular_color(color, ks, normal, light_vector):
-    P = scalar_mult_two(normal, dot_product(normal, light_vector))
-    R = subtract_vector(scalar_mult_two(P, 2), light_vector)
-    view_vector = [0, 0, 1]
+def diffuse_color(normal, shading, color):
+    #color * kd * max(0, dot_product(normal, light_vector))
+    const = shading["constants"]["constants"]
+    diff_const = [ const["red"][1], const["green"][1], const["blue"][1] ]
 
-    exp = 120
-    return color * ks * pow(max(0, dot_product(R, view_vector)), exp)
+    normal = normalize(normal)
+
+    total_diffuse = [0, 0, 0]
+    for entry in shading["light"]:
+        light_src = shading["light"][entry]
+        source = light_src['color'] 
+        location = light_src['location'] 
+        dot_prod = dot_product(normal, location)
+
+        diffuse = [ source[0] * diff_const[0] * dot_prod,
+                    source[1] * diff_const[1] * dot_prod,
+                    source[2] * diff_const[2] * dot_prod ]
+        total_diffuse = [ total_diffuse[0] + diffuse[0],
+                          total_diffuse[1] + diffuse[1],
+                          total_diffuse[2] + diffuse[2] ]
+    return total_diffuse
+def specular_color(normal, shading, color):
+    #P = scalar_mult_two(normal, dot_product(normal, light_vector))
+    #R = subtract_vector(scalar_mult_two(P, 2), light_vector)
+    #return color * ks * pow(max(0, dot_product(R, view_vector)), exp)
+
+    const = shading["constants"]["constants"]
+    specular_const = [ const["red"][1], const["green"][1], const["blue"][1] ]
+
+    normal = normalize(normal)
+
+    total_specular = [0, 0, 0]
+    for entry in shading["light"]:
+        light_src = shading["light"][entry]
+        source= light_src["color"]
+        location = light_src["location"]
+        dot_prod = dot_product(normal, location)
+
+        a = [2 * dot_prod * x for x in normal]
+        b = [ a[0] - location[0],
+              a[1] - location[1],
+              a[2] - location[2] ]
+        b = normalize(b)
+        view = normalize([1, 1, 1])
+        c = dot_product(b, view)
+        
+        specular_r = source[0] * specular_const[0] * c
+        specular_g = source[1] * specular_const[1] * c
+        specular_b = source[2] * specular_const[2] * c
+
+        total_specular = [ total_specular[0] + specular_r,
+                           total_specular[1] + specular_g,
+                           total_specular[2] + specular_b ]
+
+    return total_specular
